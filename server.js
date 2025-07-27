99b44090b7dbb9e83c66f2d35a5ae50b214f56fd
yuriiavr <94492796+yuriiavr@users.noreply.github.com>
2025-07-27 21:57:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const { DB_HOST, PORT = 3001 } = process.env;
 
 mongoose.set('strictQuery', true)
 
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok', message: 'Server is running and healthy.' });
+});
+
 mongoose.connect(DB_HOST)
   .then(() => app.listen(PORT, () => {
     console.log(`Database connection successful at port ${PORT}`)
